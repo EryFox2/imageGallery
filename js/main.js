5a76d73d572f40d2788c7b351fb8dd569c77c46e
Erykah <erykahfox25@gmail.com>
2021-11-10 12:40:49 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,127 @@
+posts = [
+    {
+        image: 'Image/IMG_0085.JPG',
+        caption: 'This is a pink flower.',
+        comments: []
+    },
+    {
+        image: 'Image/IMG_0140.JPG',
+        caption: 'This is a feet in the grass.',
+        comments: []
+    },
+    {
+        image: 'Image/IMG_0108.JPG',
+        caption: 'This is a potato.',
+        comments: []
+    }
+]
+
+
+counter = -1
+maximum = posts.length
+
+
+const nextbut = document.getElementById('nextbut')
+const prevbut = document.getElementById('prebut')
+const imageElement = document.getElementById('galleryPic')
+const captionElemet = document.getElementById('captxt')
+const commentButton = document.getElementById('combutt')
+const commentInput = document.getElementById('comTxtBox')
+
+
+nextbut.onclick = goToNext
+prevbut.onclick = goToPrev 
+commentButton.onclick = commentText
+
+
+function goToNext(){
+   if (counter <= maximum){
+        counter++
+        readData()
+   }
+   limitCheck()
+}
+
+function goToPrev(){
+   if (counter >= 0){
+       counter--
+       readData()
+   }
+   limitCheck()
+}
+
+function readData(){
+    imageElement.src = posts[counter].image
+    captionElemet.innerHTML = posts[counter].caption
+    const commentsStack = posts[counter].comments
+
+   const commentP = document.getElementById('commentParent')
+
+    const commentH = commentsStack.map((item) => {
+        return `
+                <div style="padding-bottom: 30px;" class="toTheSide">
+                    <img class="dp" src='Image/dp6.jpg'"/>
+                    <div>
+                        <div style="display:flex; flex-direction: row;">
+                            <p style="font-weight: bold; padding-right:10px;">Amandla</p> 
+                            <p>${item.time}</p>
+                        </div>
+                        <p>${item.comment}</p>
+                    </div>
+                </div>
+        `
+    })
+
+    commentP.innerHTML = commentH.join('')
+    
+    limitCheck()
+}
+
+function limitCheck(){
+    nextbut.style.display = 'block'
+    prevbut.style.display = 'block'
+
+    if (counter >= maximum - 1){
+        nextbut.style.display = 'none'
+        prevbut.style.display = 'block'
+    }
+
+    else if (counter <= 0){
+        nextbut.style.display = 'block'
+        prevbut.style.display = 'none'
+    }
+}
+
+
+
+
+commentInput.addEventListener("keyup", function(event){
+    if (event.keyCode === 13){
+        event.preventDefault();
+        commentButton.click();
+    }
+});
+
+
+function commentText(){
+    let commenttxt = commentInput.value.trim()
+    if (!commenttxt){
+        alert('please enter a comment')
+        return
+    }
+
+    const userdate = new Date()
+    const usertime =  userdate.getHours() + ':' + userdate.getMinutes()
+    userComment = {
+        comment : commenttxt,
+        time : usertime
+    }
+    posts[counter].comments.push(userComment)
+    commentInput.value = ''
+    
+    readData()
+}
+
+
+window.onload = goToNext
+
